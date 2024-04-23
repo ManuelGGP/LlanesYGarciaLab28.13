d54584713653ade80b9653b76c4eb6058fd790a8
Manny Garcia <144474323+ManuelGGP@users.noreply.github.com>
2024-04-22 21:10:26 -0500

    Update ItemToPurchase.h
    Updated Code to function. Added setters and getters.

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -4,6 +4,23 @@
 #include <string>
 using namespace std;
 
-/* Type your code here */
+class ItemToPurchase {
+private:
+    string itemName;
+    int itemPrice;
+    int itemQuantity;
+
+public:
+    ItemToPurchase();
+    
+    string GetName() const;
+    int GetPrice() const;
+    int GetQuantity() const;
+    
+    void SetName(const string &name);
+    void SetPrice(int price);
+    void SetQuantity(int quantity);
+
+};
 
 #endif
